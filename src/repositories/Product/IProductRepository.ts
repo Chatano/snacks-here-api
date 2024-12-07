9658274b9e62cd39aeee34995c66219f1e8683a5
Chatano <caepradiee@gmail.com>
2024-12-07 17:08:00 -0300

    feat: product repository interface

--- src/repositories/Product/IProductRepository.ts
+++ src/repositories/Product/IProductRepository.ts
@@ -1,9 +1,10 @@
 import { Product } from "@entities/Product";
+import { AddProductDTO } from "src/dtos/AddProductDTO";
 
 export interface IProductRepository {
-  add: () => Promise<void>,
+  add: (data: AddProductDTO) => Promise<Product>,
   getAll: () => Promise<Product[]>,
   getByID: (product_id: number) => Promise<Product | null>,
   delete: (product_id: number) => Promise<void>,
-  edit: (product_id: number) => Promise<void>,
+  edit: (id: number, updatedFields: Partial<AddProductDTO>) => Promise<Product>,
 }
