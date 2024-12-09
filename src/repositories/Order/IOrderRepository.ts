fe127aedfece276f38c7015628bccb1e95929bb6
Chatano <caepradiee@gmail.com>
2024-12-09 15:57:31 -0300

    feat: add changeOrderStatus to orderRepository

--- src/repositories/Order/IOrderRepository.ts
+++ src/repositories/Order/IOrderRepository.ts
@@ -1,8 +1,8 @@
 import type { RegisterOrderDTO } from '@dtos/RegisterOrderDTO'
-import type { Order } from '@entities'
-import type { ORDER_STATUS } from '@prisma/client'
+import type { Order, ORDER_STATUS } from '@entities'
 
 export interface IOrderRepository {
   add: (dto: RegisterOrderDTO) => Promise<Order>
+  changeOrderStatus: (order_id: number, status: ORDER_STATUS) => Promise<Order>
   getOrders: (status?: ORDER_STATUS) => Promise<Order[]>
 }
