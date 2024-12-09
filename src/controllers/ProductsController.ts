a72f2b1272be4a51128e5630b7693b63cac74394
Chatano <caepradiee@gmail.com>
2024-12-09 15:22:26 -0300

    feat: import useCases into controller

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,82 +1,33 @@
-import { ProductService } from '@services/ProductService'
 import type { Request, Response } from 'express'
-import { container } from 'tsyringe'
-import { z } from 'zod';
-
-class Schemas {
-  public static idParam = z.object({
-    id: z.string().regex(/^\d+$/, "Invalid ID.").transform(Number)
-  })
-  public static createProduct = z.object({
-    name: z.string().min(1, 'Name is required.'),
-    price: z.number().positive('Price must be greater than zero.')
-  })
-  public static editProduct = z.object({
-    id: z.string().regex(/^\d+$/, "Invalid ID.").transform(Number),
-    name: z.string().optional(),
-    price: z.number().positive().optional()
-  })
-}
+import { CreateProductUseCase } from '@useCases/Products/CreateProductUseCase';
+import { GetAllProductsUseCase } from '@useCases/Products/GetAllProductsUseCase';
+import { GetProductByIDUseCase } from '@useCases/Products/GetProductByIDUseCase';
+import { EditProductUseCase } from '@useCases/Products/EditProductUseCase';
+import { DeleteProductUseCase } from '@useCases/Products/DeleteProductUseCase';
 
 export class ProductsController {
   public static async GetAllProducts(req: Request, res: Response) {
-    const service = container.resolve(ProductService);
-
-    const products = await service.getAll()
+    const products = await GetAllProductsUseCase.execute();
     res.status(200).json({ products })
   }
 
   public static async GetProductByID(req: Request, res: Response) {
-    const { success, data, error } = Schemas.idParam.safeParse(req.params);
-
-    if (!success) {
-      res.status(400).json({ error: error?.errors })
-      return ;
-    }
-
-    const service = container.resolve(ProductService);
-    const product = await service.getByID(data.id)
+    const product = await GetProductByIDUseCase.execute({ id: req.params.id });
     res.status(200).json({ product })
   }
 
   public static async CreateProduct(req: Request, res: Response) {
-    const { success, data, error } = Schemas.createProduct.safeParse(req.body)
-
-    if (!success) {
-      res.status(400).json({ error: error?.errors })
-      return ;
-    }
-
-    const service = container.resolve(ProductService);
-    const product = await service.addProduct(data)
-    res.status(201).json({ product })
+    const product = await CreateProductUseCase.execute(req.body);
+    res.status(201).json({ product });
   }
 
   public static async EditProduct(req: Request, res: Response) {
-    const { success, data, error } = Schemas.editProduct.safeParse(req.body);
-  
-    if (!success) {
-      res.status(400).json({ error: error?.errors })
-      return ;
-    }
-
-    const service = container.resolve(ProductService);
-    
-    const { id, name, price } = data
-    const product = await service.edit(id, { name, price });
-    res.status(200).json({ product });
+    const product = await EditProductUseCase.execute(req.body);
+    res.status(201).json({ product });
   }
   
   public static async DeleteProduct(req: Request, res: Response) {
-    const { success, data, error } = Schemas.idParam.safeParse(req.params);
-
-    if (!success) {
-      res.status(400).json({ error: error?.errors })
-      return ;
-    }
-
-    const service = container.resolve(ProductService);
-    await service.delete(data.id)
+    await DeleteProductUseCase.execute({ id: req.params.id });
     res.status(204).send()
   }
 }
